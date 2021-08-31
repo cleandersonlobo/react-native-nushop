29e360eb7442d296a4df00176ca13533915463b9
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-08-31 01:01:07 -0300

    feat: initial navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import HomeScreen from 'screens/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start w orking on your app!</Text>
-      <StatusBar />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
